d207eafec9e813e6ef052caa148d4bd2818a434c
Izwor <izwor142@wp.pl>
2016-11-10 23:02:09 +0100

    testing program

---
+++ test1.cpp
@@ -0,0 +1,6 @@
+Script started on pon, 25 lip 2016, 21:38:48
+]0;izwor@izwor-CR61-2M-CX61-2OC-CX61-2OD: ~/catkin_ws/src/rosaria_examples[01;32mizwor@izwor-CR61-2M-CX61-2OC-CX61-2OD[00m:[01;34m~/catkin_ws/src/rosaria_examples[00m$ ^C
+]0;izwor@izwor-CR61-2M-CX61-2OC-CX61-2OD: ~/catkin_ws/src/rosaria_examples[01;32mizwor@izwor-CR61-2M-CX61-2OC-CX61-2OD[00m:[01;34m~/catkin_ws/src/rosaria_examples[00m$ ex[K[Kexit'[K
+exit
+
+Script done on pon, 25 lip 2016, 21:39:15
